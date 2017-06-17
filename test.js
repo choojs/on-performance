c37ab8611d9e180161a02c102021e00ec6687f7e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-06-17 12:28:29 +0200

    fix tests

--- test.js
+++ test.js
@@ -4,28 +4,34 @@ var tape = require('tape')
 var onPerformance = require('./')
 
 tape('should flush already buffered events', function (assert) {
-  assert.plan(3) // bundle.js, bundle.css and start event
+  var startLength = window.performance.getEntries().length
   var i = 0
   var stop = onPerformance(function (entry) {
     assert.ok(entry, 'entry passed')
-    if (i++ === 2) stop()
+    i++
+    endMaybe()
   })
+
+  endMaybe() // call once at the start to if startLength === 0
+  function endMaybe () {
+    assert.pass('endMaybe called')
+    if (i < startLength) return
+    if (stop) stop()
+    assert.end()
+  }
 })
 
 tape('should capture performance measures', function (assert) {
   assert.plan(2)
 
   var startLength = window.performance.getEntries().length
-  var firstIgnored = false
 
   var stop = onPerformance(function (entry) {
-    if (!firstIgnored) {
-      firstIgnored = true
-      return
-    }
+    if (entry.entryType !== 'measure') return
+
+    assert.ok(entry, 'entry was passed')
     var newLength = window.performance.getEntries().length
     assert.equal(startLength, newLength, 'entries are cleared')
-    assert.equal(entry.entryType, 'measure')
     stop()
   })
 
