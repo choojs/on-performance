7e10c20256d18ec387e005f9a315ecca19a134ca
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-12-11 18:50:48 +0900

    singleton

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var scheduler = require('nanoscheduler')()
+var onIdle = require('on-idle')
 var assert = require('assert')
+
+var entryTypes = [
+  'frame',
+  'measure',
+  'navigation',
+  'resource',
+  'longtask'
+]
 
 module.exports = onPerformance
 
@@ -9,48 +17,34 @@ function onPerformance (cb) {
   var PerformanceObserver = typeof window !== 'undefined' && window.PerformanceObserver
   if (!PerformanceObserver) return
 
-  var observer = new PerformanceObserver(handler)
-  observer.observe({
-    entryTypes: [
-      'frame',      // not implemented yet in any browser
-      'measure',
-      'navigation',
-      'resource',
-      'longtask'
-    ]
-  })
+  // Enable singleton.
+  if (window._onperformance) return window._onperformance.push(cb)
 
-  window.performance.getEntries().forEach(function (entry) {
-    scheduler.push(function () {
-      cb(entry)
-      clear(entry)
-    })
-  })
+  window._onperformance = [cb]
+  var observer = new PerformanceObserver(parseEntries)
+  setTimeout(function () {
+    parseEntries(window.performance)
+    observer.observe({ entryTypes: entryTypes })
+  }, 0)
+  return observer.disconnect.bind(observer)
 
-  function handler (list) {
+  function parseEntries (list) {
     list.getEntries().forEach(function (entry) {
-      scheduler.push(function () {
-        cb(entry)
+      onIdle(function () {
+        window._onperformance.forEach(function (cb) {
+          cb(entry)
+        })
         clear(entry)
       })
     })
   }
-
-  // Workaround to prevent the observer instance from being garbage collected
-  // https://twitter.com/choojs/status/876098840495091713
-  window['obs' + window.performance.now()] = observer
-
-  return observer.disconnect.bind(observer)
 
   // Navigation, longtask and frame don't have a clear method (yet)
   // Resource timings can only be cleared in bulk
   // see: https://developer.mozilla.org/en-US/docs/Web/API/Performance/clearMeasures
   function clear (entry) {
     var type = entry.entryType
-    if (type === 'measure') {
-      window.performance.clearMeasures(entry.name)
-    } else if (type === 'resource') {
-      window.performance.clearResourceTimings()
-    }
+    if (type === 'measure') window.performance.clearMeasures(entry.name)
+    else if (type === 'resource') window.performance.clearResourceTimings()
   }
 }
