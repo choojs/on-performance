97b435df23d1c276c530220e4c894d7945c4baeb
Irina Shestak <lrlna@users.noreply.github.com>
2018-01-24 12:41:06 +0800

    get on-performance to work in node env (#5)
    * get on-performance to work in node env
    
    * run travis on node 8.5 or higher
    
    * get rid off dependency checker as well
    
    * don't use minor ranges for travis
    
    * electron support in index.js

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-var scheduler = require('nanoscheduler')()
 var assert = require('assert')
 
 var entryTypes = [
-  'frame',
+  'node',
+  'mark',
   'measure',
-  'navigation',
-  'resource',
-  'longtask'
+  'gc',
+  'function'
 ]
 
 module.exports = onPerformance
 
 function onPerformance (cb) {
+  if (typeof window !== 'undefined') return require('./browser.js')(cb) // electron support
+
   assert.equal(typeof cb, 'function', 'on-performance: cb should be type function')
 
-  var PerformanceObserver = typeof window !== 'undefined' && window.PerformanceObserver
-  if (!PerformanceObserver) return
+  var PerformanceObserver
+  var performance
+  try {
+    PerformanceObserver = require('perf_hooks').PerformanceObserver
+    performance = require('perf_hooks').performance
+  } catch (e) {
+    return cb(new Error('on-performance: PerformanceObserver is unavailable. Use Node v8.5.0 or higher.'))
+  }
 
   // Enable singleton.
-  if (window._onperformance) {
-    window._onperformance.push(cb)
+  if (global._onperformance) {
+    global._onperformance.push(cb)
     return stop
   }
 
-  window._onperformance = [cb]
+  global._onperformance = [cb]
   var observer = new PerformanceObserver(parseEntries)
   setTimeout(function () {
-    parseEntries(window.performance)
+    parseEntries(performance)
     observer.observe({ entryTypes: entryTypes })
   }, 0)
 
   return stop
 
   function stop () {
-    window._onperformance.splice(window._onperformance.indexOf(cb), 1)
+    global._onperformance.splice(global._onperformance.indexOf(cb), 1)
   }
 
   function parseEntries (list) {
     list.getEntries().forEach(function (entry) {
-      scheduler.push(function () {
-        clear(entry)
-        window._onperformance.forEach(function (cb) {
-          cb(entry)
-        })
+      clear(entry)
+      global._onperformance.forEach(function (cb) {
+        cb(entry)
       })
     })
   }
 
-  // Navigation, longtask and frame don't have a clear method (yet)
-  // Resource timings can only be cleared in bulk
-  // see: https://developer.mozilla.org/en-US/docs/Web/API/Performance/clearMeasures
   function clear (entry) {
     var type = entry.entryType
-    if (type === 'measure') window.performance.clearMeasures(entry.name)
-    else if (type === 'resource') window.performance.clearResourceTimings()
+    if (type === 'measure') performance.clearMeasures(entry.name)
+    else if (type === 'mark') performance.clearMarks(entry.name)
+    else if (type === 'function') performance.clearFunctions(entry.name)
   }
 }
